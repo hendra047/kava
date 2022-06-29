0719a508ac11d337b1e231bf7ad9a7c76136bd92
desmondkd <desmondneth@gmail,com>
2022-06-29 16:13:34 +0700

    small update(2) -kenneth

--- app/src/main/java/com/ubaya/kava/util/Util.kt
+++ app/src/main/java/com/ubaya/kava/util/Util.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
